0199c5714013b070df13685cfdbeb85ae846c50b
Your Name <you@example.com>
2024-02-19 15:02:17 +0300

    Добавление функции добавления администратора

--- forum/admin.go
+++ forum/admin.go
@@ -1,1 +1,22 @@
-struct 
+package main
+
+import "fmt"
+
+type admin struct {
+	name string
+	age  int
+	post string
+}
+
+func main() {
+	x := admin{
+		name: "Victor",
+		age:  15,
+		post: "Administrator of shop",
+	}
+	fmt.Println(x)
+}
+
+func add(name, age, post) {
+
+}
