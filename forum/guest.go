5b12d6bfab49d65a96c026a7eb5270a56a48659d
Your Name <you@example.com>
2024-02-20 13:59:56 +0300

    Добавление функции уведомления при выходе неавторизированного пользователя и добавление логгирования

--- forum/guest.go
+++ forum/guest.go
@@ -1,7 +1,12 @@
 
+func outMessage(){
+	fmt.Println("До скорой встречи! Ждем Вас снова!")
+	log.Println("Пользователь покинул сайт")
+}
 
 func intoMessage(){
 	fmt.Println("Спасибо за то,что зашли к нам. Посещайте нас почаще\nА лучше заругистрируйтесь")
+	log.Println("Пользователь вошел на сайт")
 }
 
 func main(){
@@ -11,7 +16,7 @@ func main(){
 		intoMessage()
 	}
 	if choice == "out"{
-
+		outMessage()
 	}
 
 }
