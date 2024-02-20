f34b1bc8dfa74edcd41799162039a842ec6defe2
Your Name <you@example.com>
2024-02-20 17:59:28 +0300

    Реализация механизма покупки на торговой площадке для незарегистрированных пользователей

--- forum/guest.go
+++ forum/guest.go
@@ -14,7 +14,18 @@ func intoMessage(){
 	fmt.Println("Спасибо за то,что зашли к нам. Посещайте нас почаще\nА лучше заругистрируйтесь")
 	log.Println("Пользователь вошел на сайт")
 }
-
+func buyFunc() {
+	i := "0"
+	fmt.Scanf("%s",&i)
+	switch i {
+	case "Game Account":
+		fmt.Println("Покупка игрового аккаунта")
+	case "Game Currency":
+		fmt.Println("Покупка игровой валюты")
+	case "Facebook Account":
+		fmt.Println("Покупка аккаунта Facebook")
+	}
+}
 func main(){
 	var choice
 	fmt.Scanf("%s",&choice)
@@ -24,5 +35,9 @@ func main(){
 	if choice == "out"{
 		outMessage()
 	}
+	if choice == "cart"{
+		buyFunc()
+	}
 
 }
+
