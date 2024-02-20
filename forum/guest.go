585796e53ba1871ad6d8d8cd9c20648d5b4b733e
Your Name <you@example.com>
2024-02-20 13:56:39 +0300

    Добавление функции уведомления при входе без авторизации

--- forum/guest.go
+++ forum/guest.go
@@ -1,13 +1,14 @@
 
 
-
+func intoMessage(){
+	fmt.Println("Спасибо за то,что зашли к нам. Посещайте нас почаще\nА лучше заругистрируйтесь")
+}
 
 func main(){
 	var choice
 	fmt.Scanf("%s",&choice)
 	if choice == "into"{
-
-
+		intoMessage()
 	}
 	if choice == "out"{
 
