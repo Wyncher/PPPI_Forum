f42581aeb41dc21a9410457f3e8ad841f107d574
Your Name <you@example.com>
2024-02-20 18:03:49 +0300

    Добавление системы вознаграждения за посещение сайта для неавторизированных пользователей

--- forum/viewer.go
+++ forum/viewer.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 )
+
+var balR = 0
 
 func outMessage(){
 	fmt.Println("До скорой встречи! Ждем Вас снова!")
@@ -13,6 +16,10 @@ func outMessage(){
 func intoMessage(){
 	fmt.Println("Спасибо за то,что зашли к нам. Посещайте нас почаще\nА лучше заругистрируйтесь")
 	log.Println("Пользователь вошел на сайт")
+	timer1 := time.NewTimer(2 * time.Second)
+	<-timer1.C
+	log.Println("Начислено 2 рубля за посещение сайта")
+	fmt.Println("Вам начислено 2 рубля за посещение сайта\nЧтобы получить их нужно зарегистрироваться")
 }
 
 func main(){
