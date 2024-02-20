f3fcf0142d60e05285c7d6d63e5331cc21b02fbd
Your Name <you@example.com>
2024-02-20 17:54:26 +0300

    Изменение функции добавления модераторов и проверка функции

--- forum/moder.go
+++ forum/moder.go
@@ -2,17 +2,22 @@ package main
 
 import "fmt"
 
+var moderList = []
+
 type moder struct {
 	name string
 	age  int
 	post string
 }
 
+
 func main() {
-	x := moder{
-		name: "Victor",
-		age:  15,
-		post: "Moderator of shop",
-	}
-	fmt.Println(x)
+
+	add("Pavlik",22,"Moderator of tree 'Флудилка'")
+	add("Artemon887",17,"Moderator of tree 'Компьютерные игры'")
+}
+
+func add(name, age, post) {
+	a := moder{name, age, post}
+	adminList = append(adminList,a)
 }
